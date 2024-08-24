3fea041fdbd045b78b16e2b19f16cb379019d3ec
zcxbin <zcxbin@gmail.com>
2024-08-24 15:00:47 +0700

    update

---
+++ books.py
@@ -0,0 +1,76 @@
+from fastapi import FastAPI, Body
+
+app = FastAPI()
+
+BOOKS = [
+    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
+    {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
+    {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
+    {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
+    {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
+    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
+]
+
+
+@app.get('/books')
+async def read_all_book():
+    return BOOKS
+
+
+@app.get('/books/{book_title}')
+async def read_book(book_title: str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
+
+
+@app.get('/books/')
+async def read_category_by_query(category: str):
+    book_to_return = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            book_to_return.append(book)
+    return book_to_return
+
+
+# Đem '/books/by_author/' lên trên '/books/{book_author}/' bởi vì conflict (category: str) khi nhập author thì sẽ bị
+# missing category
+@app.get('/books/by_author/')
+async def read_book_by_author_path(author: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == author.casefold():
+            books_to_return.append(book)
+
+    return books_to_return
+
+
+@app.get('/books/{book_author}/')
+async def read_author_category_by_query(book_author: str, category: str):
+    book_to_return = []
+    for book in BOOKS:
+        if (book.get('author').casefold() == book_author.casefold() and
+                book.get('category').casefold() == category.casefold()):
+            book_to_return.append(book)
+
+    return book_to_return
+
+
+@app.post('/books/create_book')
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
+
+
+@app.put('/books/update_book')
+async def update_book(updated_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
+            BOOKS[i] = updated_book
+
+
+@app.delete('/books/delete_book/{book_title}')
+async def delete_book(book_title: str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == book_title.casefold():
+            BOOKS.pop(i)
+            break
